855b8c5d2b00319303862c3c3e0d69c6381ce9de
He Chen <chenhe95@hotmail.com>
2015-12-02 21:18:18 -0500

    supports has actor X acted in same movie as actor Y
    also some changes in loadIndex to be used later (for example, load
    random actors/movies)

--- TRIVIA/src/DBConnect.java
+++ TRIVIA/src/DBConnect.java
@@ -30,24 +30,28 @@ public class DBConnect {
 
 	public static void main(String[] args) throws SQLException {
 		String testQuery;
-		String preQuery = "(SELECT * from (SELECT @row := @row +1 as rownum, name as name, mid as mid, rating as rating FROM (SELECT @row := 0) r, movie.movies) ranked WHERE rownum % 4 = 1 AND rating > 3)";
+		String preQuery = "(SELECT * from (SELECT @row := @row +1 as rownum, aid as aid, mid as mid FROM (SELECT @row := 0) r, movie.acts) ranked WHERE rownum % 1 = 0 order by mid desc limit 50000, 200000)";
 		//testQuery = "select movie.movie_genre.mid as m_id, movie.movie_genre.genre as m_genre from movie.movie_genre inner join movie.movies on movie.movies.mid = movie.movie_genre.mid where movie.movies.rating > 4";
 		testQuery = "select movie.movie_genre.genre as m_genre, T.name as m_name from movie.movie_genre inner join " + preQuery + "as T on T.mid = movie.movie_genre.mid";
 		//testQuery = "select count(*) as cn from movie.actors as A where A.Date_of_birth LIKE '%%%%-%%-%%'";
 		testQuery = "select * from (select @row := @row +1 as rownum, A.name as a_name, A.Date_of_birth as a_DOB from (SELECT @row := 0) r, movie.actors as A where A.Date_of_birth like '%%%%-%%-%%') ranked where rownum % 380 = 1";
 		// select T.a_name, T.a_DOB from (select @row := @row +1 as rownum, A.name as a_name, A.Date_of_birth as a_DOB from (SELECT @row := 0) r, movie.actors as A) as T ranked WHERE rownum % 4 = 1"
+		testQuery = preQuery;
 		//+ ") ranked WHERE rownum % " + skip + " = 1
 		Connection connection = getConnection(); //A.name as a_name, A.Date_of_birth // <> 'null' and A.Date_of_birth <> '' and A.Date_of_birth <> ' '
 		Statement statement = connection.createStatement();
 		statement.executeQuery(testQuery);
 		ResultSet result = statement.getResultSet();
+		int i = 0;
 		while (result.next()) {
+			i++;
 			//int mid = result.getInt("mid");
 			//double rating = result.getDouble("rating");
 			//System.out.println("" + result.getString("m_name") + " " + result.getString("m_genre"));
-			System.out.println(result.getString("a_name") + " " + result.getString("a_DOB"));
+			System.out.println(result.getString("mid") + " " + result.getString("aid"));
 			//System.out.println(result.getInt("cn"));
 		}
+		System.out.println("TOTAL: " + i);
 	}
 
 	public static Connection getConnection() throws SQLException {
---
+++ TRIVIA/src/MongoDBConnect.java
@@ -0,0 +1,14 @@
+
+public class MongoDBConnect {
+	private static final String dbms = "mysql";
+	private static final String dbname = "movie";
+	private static final String username = "fanglinlu";
+	private static final String password = "lfl383LFL";
+	private static final int portNumber = 3306;
+	private static final String servername = "trivia.crqsulbfdc9h.us-west-2.rds.amazonaws.com";
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
