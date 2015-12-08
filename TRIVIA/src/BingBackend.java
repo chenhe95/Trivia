0931c415df92a5f832109b017279ce5c213f78c7
jasontsh <tangsh@sas.upenn.edu>
2015-12-08 18:54:15 -0500

    new api for bing

--- TRIVIA/src/BingBackend.java
+++ TRIVIA/src/BingBackend.java
@@ -1,14 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
 
-import com.google.code.bing.search.client.BingSearchClient;
-import com.google.code.bing.search.client.BingSearchClient.SearchRequestBuilder;
-import com.google.code.bing.search.client.BingSearchServiceClientFactory;
-import com.google.code.bing.search.schema.AdultOption;
-import com.google.code.bing.search.schema.SearchOption;
-import com.google.code.bing.search.schema.SearchResponse;
-import com.google.code.bing.search.schema.SourceType;
-import com.google.code.bing.search.schema.web.WebResult;
-import com.google.code.bing.search.schema.web.WebSearchOption;
+import net.billylieurance.azuresearch.AzureSearchResultSet;
+import net.billylieurance.azuresearch.AzureSearchWebQuery;
+import net.billylieurance.azuresearch.AzureSearchWebResult;
+
 
 /**
  * 
@@ -23,30 +19,23 @@ public class BingBackend {
 	public static final String APP_KEY = "0AZbBkuj8OHRk+UduutcFfxRbHSY2Aimi8nP5CK8gNU=";
 	
 	/**
-	 * In order to use the webresults, it has getTitle(), getDescription(), getUrl(), and getDateTime().
+	 * In order to use the webresults, it has getTitle(), getDescription(), getDisplayUrl(), getUrl().
 	 * @param keyword the word to be queried by bing
+	 * @param size the size of the response
 	 * @return a list of results.  
 	 */
-	public static List<WebResult> getQueryResult(String keyword){
-		BingSearchServiceClientFactory factory = BingSearchServiceClientFactory.newInstance();
-		BingSearchClient client = factory.createBingSearchClient();
-
-		SearchRequestBuilder builder = client.newSearchRequestBuilder();
-		builder.withAppId(APP_KEY);
-		builder.withQuery(keyword);
-		builder.withSourceType(SourceType.WEB);
-		builder.withVersion("2.0");
-		builder.withMarket("en-us");
-		builder.withAdultOption(AdultOption.MODERATE);
-		builder.withSearchOption(SearchOption.ENABLE_HIGHLIGHTING);
-
-		builder.withWebRequestCount(10L);
-		builder.withWebRequestOffset(0L);
-		builder.withWebRequestSearchOption(WebSearchOption.DISABLE_HOST_COLLAPSING);
-		builder.withWebRequestSearchOption(WebSearchOption.DISABLE_QUERY_ALTERATIONS);
-
-		SearchResponse response = client.search(builder.getResult());
-		return response.getWeb().getResults();
+	public static List<AzureSearchWebResult> getQueryResult(String keyword, int size){
+		List<AzureSearchWebResult> answer = new ArrayList<>((size+50));
+		AzureSearchWebQuery aq = new AzureSearchWebQuery();
+		aq.setAppid(APP_KEY);
+		aq.setQuery(keyword);
+		
+		for (int i = 1; i <= size/50+1; i++){
+			AzureSearchResultSet<AzureSearchWebResult> ars = aq.getQueryResult();
+			for (AzureSearchWebResult anr : ars) {
+				answer.add(anr);
+			}
+		}
+		return answer;
 	}
-	
 }
